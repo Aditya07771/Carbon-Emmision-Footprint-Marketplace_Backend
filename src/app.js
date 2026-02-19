4a02b150a0287115f235eed5faa5f8bd389c4183
Aditya07771 <adityanishad753@gmail.com>
2026-02-20 03:53:09 +0530

    domain added

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(helmet({
 app.use(compression());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'], // Add your frontend local dev URLs here
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://carbon-emmision-footprint-marketpla.vercel.app'], // Add your frontend local dev URLs here
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-wallet-address', 'Accept'],
   credentials: true
